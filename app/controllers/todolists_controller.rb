c6b64467ddb812df459270045829281e9b57f73e
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-08 16:02:29 +0900

    [Start]3-5

---
+++ app/controllers/todolists_controller.rb
@@ -0,0 +1,4 @@
+class TodolistsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'todolists/new'
   get 'top' => 'homes#top'
 
 end
