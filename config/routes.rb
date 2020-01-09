52c0e9cc981ce9fddb7c494f63c5fbd2af03c919
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-09 13:45:25 +0900

    [Finish]3-9

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # .../todolists/1 や .../todolists/3 に該当する
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  delete 'todolists/:id' => 'todolists#destroy', as: 'destroy_todolist'
 
 end
