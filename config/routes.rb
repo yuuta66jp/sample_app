c9f6dcfc7eb19c25ca8a4a61fcdcfd5f2f22408d
yuuta66jp <yuuta66jp@yahoo.co.jp>
2020-01-09 11:24:51 +0900

    [Start]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
   # .../todolists/1 や .../todolists/3 に該当する
+  get 'todolists/:id/edit' => 'todolists', as: 'edit_todolist'
 
 end
